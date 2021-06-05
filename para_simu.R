aa4a932a2a3a0d95172e96eb69674f6ace686704
jw-shan <jwshan423@gmail.com>
2021-06-05 17:02:07 +0800

    uodate

--- para_simu.R
+++ para_simu.R
@@ -1,10 +1,9 @@
 rm(list=ls())
 library(parallel)
 
-source("functionals.R")
-source("TrueValue.R")
-source("Data_gen.R")
-source("plot.R")
+source("1.1 Datagen_1D.R")
+source("1.2 KernelEstimators.R")
+source("1.3 plot.R")
 
 
 ## Monto Carlo times and Sample Size###
@@ -32,37 +31,37 @@ truevalue<- 0.087
 
 
 # parallel setting
-cl <- makeCluster(50)
+cl <- makeCluster(8)
 clusterExport(cl,ls())
-# clusterEvalQ(cl,)
+
 
 ## Estimation function 
 estimation <- function(count) {
-
+  
   Data<-DataGen(N,seed+count)
-  h <- N^{-2/7}
+  h <- 1.06*sqrt(var(Data$x))* N^{-2/7}
   hopt <- 1.06*sqrt(var(Data$x))* N^{-1/5}
   X<-Data$x
   Z<-Data$z
   D<-Data$d
   Y<-Data$y
   
-  T1est <- T1(X,Y,D,Z,h)
-  T2est <- T2(X,Y,D,Z,h)
-  T3est <- T3(X,Y,D,Z,h)
-  Test  <- Tt(X,Y,D,Z,hopt)
+  KSE1_est <- KSE_1(X,Y,D,Z,h)
+  KSE2_est <- KSE_2(X,Y,D,Z,h)
+  KSE3_est <- KSE_3(X,Y,D,Z,h)
+  KSET_est <- KSE_t(X,Y,D,Z,hopt)
   # veff  <- estVeff(X,Y,D,Z,hopt)
   
   # est <- cbind(T1est,T2est,T3est,Test,veff)
-  est <- cbind(T1est,T2est,T3est,Test)
+  est <- cbind(KSE1_est,KSE2_est,KSE3_est,KSET_est)
   
-  # est <- cbind(Test,veff)
   
   return(est)
 }
 
 est  <- parSapply(cl,1:J,estimation)
 est  <- t(est)
+
 # veff <- est[,5]
 # est  <- est[,1:4]
 
@@ -90,13 +89,61 @@ for (i in 1:4) {
 
 result
 
-# summary of veff
-mean(veff)
-sd(veff)
-mean(veff/sqrt(N))
+
+# # summary of veff
+# mean(veff)
+# sd(veff)
+# mean(veff/sqrt(N))
+
 
 # plot
-est.df <- data.frame(est)
-plt(est.df)
+est.df <- data.frame(est1)
+plt_ATE(est.df)
 
 stopCluster(cl)
+
+
+# ======naive=====================================================================
+estimation <- function(count) {
+  
+  Data<-DataGen(N,seed+count)
+  X<-Data$x
+  Z<-Data$z
+  D<-Data$d
+  Y<-Data$y
+  
+  est <- mean(Y[D==1])-mean(Y[D==0])
+  
+  # est <- cbind(Test,veff)
+  
+  return(est)
+}
+
+est  <- parSapply(cl,1:J,estimation)
+est  <- t(est)
+
+result <- matrix(nrow = 1, ncol = 5)
+colnames(result)<-c("bias","stdev","MSE","RMSE","CR")
+rownames(result)<-"naive"
+
+Delta <- mean(est)
+bias  <- Delta - truevalue
+mse   <- 1/J*(sum((est-truevalue)^2))
+stdev <- sqrt(1/J*(sum((est-Delta)^2)))
+rmse<- sqrt(mse)
+
+count<-0
+for(j in 1:J){
+  if(est[j]> truevalue-1.96*stdev & est[j]< truevalue+1.96*stdev)
+    count<- count+1
+}
+coverage_rate <<- count/J
+CR <- coverage_rate
+
+result <- cbind(bias,stdev,mse,rmse,CR)
+
+
+result
+
+
+
